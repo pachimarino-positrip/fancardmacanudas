0c4cce8cb0e27291d812688e69409094e711abff
Pachi Marino <pachimarino@gmail.com>
2026-04-08 11:08:17 -0300

    fix supabase server file

--- lib/supabase/server.ts
+++ lib/supabase/server.ts
@@ -1,16 +1,25 @@
+import { createServerClient } from '@supabase/ssr'
+import { cookies } from 'next/headers'
 
-Build Error
-Next.js (15.1.6) is outdated (learn more)
-Failed to compile
+export async function createClient() {
+  const cookieStore = await cookies()
 
-./app/auth/callback/route.ts:2:1
-Module not found: Can't resolve '@/lib/supabase/server'
-  1 | import { NextResponse } from 'next/server';
-> 2 | import { createClient } from '@/lib/supabase/server';
-    | ^
-  3 |
-  4 | export async function GET(request: Request) {
-  5 |   const { searchParams, origin } = new URL(request.url);
-
-https://nextjs.org/docs/messages/module-not-found
-This error occurred during the build process and can only be dismissed by fixing the error.
+  return createServerClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+    {
+      cookies: {
+        getAll() {
+          return cookieStore.getAll()
+        },
+        setAll(cookiesToSet) {
+          try {
+            cookiesToSet.forEach(({ name, value, options }) => {
+              cookieStore.set(name, value, options)
+            })
+          } catch {}
+        },
+      },
+    }
+  )
+}
