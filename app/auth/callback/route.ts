725c4cdc0015e7d2d2171f88ae8e7396b8959071
Pachi Marino <pachimarino@gmail.com>
2026-04-08 11:42:17 -0300

    fix dynamic route

--- app/auth/callback/route.ts
+++ app/auth/callback/route.ts
@@ -1,15 +1,17 @@
-import { NextResponse } from 'next/server';
-import { createClient } from '@/lib/supabase/server';
+export const dynamic = 'force-dynamic'
+
+import { NextResponse } from 'next/server'
+import { createClient } from '@/lib/supabase/server'
 
 export async function GET(request: Request) {
-  const { searchParams, origin } = new URL(request.url);
-  const code = searchParams.get('code');
-  const next = searchParams.get('next') ?? '/card';
+  const { searchParams, origin } = new URL(request.url)
+
+  const code = searchParams.get('code')
 
   if (code) {
-    const supabase = await createClient();
-    await supabase.auth.exchangeCodeForSession(code);
+    const supabase = createClient()
+    await supabase.auth.exchangeCodeForSession(code)
   }
 
-  return NextResponse.redirect(`${origin}${next}`);
+  return NextResponse.redirect(origin)
 }
